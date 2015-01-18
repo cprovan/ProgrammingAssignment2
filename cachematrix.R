b6ba23ffcbc0fb8228a6911c420fa94a322371e3
cprovan <cprovan@mosaicdatascience.com>
2015-01-18 16:38:34 -0500

    complete first versions of makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions to store, compute, and cache the inverse of a nonsingular matrix
 
-## Write a short comment describing this function
+## Function makeCacheMatrix(x) creates a list of functions to set and get
+##  the value of the matrix x and the inverse of x.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # initialize inverse to NULL
+  xinv <- NULL;
+  # set function stores matrix as x and reverts inverse to NULL
+  set <- function(y) {
+    x <<- y;
+    xinv <<- NULL;
+  }
+  # get function returns matrix
+  get <- function() x
+  # setinv function stores inverse matrix as xinv
+  setinv <- function(inv) xinv <<- inv;
+  # getinv returns inverse matrix
+  getinv <- function() xinv
+  # return list of functions
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve returns the cached inverse matrix if it exists and computes
+##   and caches the inverse matrix otherwise
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get cached inverse
+  xinv <- x$getinv();
+  # if cached inverse is not NULL then return this matrix
+  if(!is.null(xinv)) {
+    message("returning cached inverse");
+    return(xinv);
+  }     
+  # if cached inverse is NULL then compute, cache, and return inverse
+  xinv <- solve(x$get());
+  x$setinv(xinv);
+  xinv
 }
